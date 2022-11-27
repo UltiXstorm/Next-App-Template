67bea773bc574f3218d5e03fade7893ab7beab2e
UltiXstorm <ultixstorm@gmail.com>
2022-11-27 23:38:27 +0100

    save

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('Homepage had a title like Create Next App', async ({ page }) => {
-  await page.goto('https://localhost:3000/');
+
+  await page.goto('http://localhost:3000/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Create Next App/);
--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,6 @@ const config: PlaywrightTestConfig = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000/',
     browserName: 'chromium',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -73,7 +72,6 @@ const config: PlaywrightTestConfig = {
      command: 'pnpm serve',
      port: 3000,
      timeout: 120 * 1000,
-     reuseExistingServer: !process.env.CI
    },
 };
 
