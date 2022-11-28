4639a71310c552491e0ca5d872516acab4848bcf
UltiXstorm <ultixstorm@gmail.com>
2022-11-28 23:56:37 +0100

    save

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,9 +1,10 @@
 import { test, expect } from '@playwright/test';
 
+test.beforeEach(async ({ page }) => {
+  await page.goto('/');
+});
 
-test('Title render', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-
+test('title render', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Create Next App/);
 });
