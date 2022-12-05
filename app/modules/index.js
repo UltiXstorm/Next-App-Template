236de5fa5bd6a9603cd60a29d688ea107fd70c99
UltiXstorm <ultixstorm@gmail.com>
2022-12-05 22:41:40 +0100

    save new orga

--- app/modules/index.js
+++
@@ -1,3 +0,0 @@
-export { Landing } from './landing/landing.component';
-export { Main } from './main/main.component';
-export { Navbar } from './navbar/navbar.component';
--- app/modules/main/routes.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Landing } from '../landing/landing.component';
-
-export const mainRoutes = [
-  {
-    path: '/',
-    element: <Landing />,
-  },
-];
